74bba925d735ab7ad0d1769fe019b69aa489524d
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-11-12 12:48:00 +0600

    Update FaceRec.py

--- FaceRec.py
+++ FaceRec.py
@@ -1,1 +1,9 @@
+#FaceRecognition App
 
+import cv2
+import face_recognition
+import numpy as np
+import os
+
+#Path to folder with known faces (add images like 'person1.jpg', 'person2.jpg', 'person3.jpg')
+KNOWN_FACES_DIR
